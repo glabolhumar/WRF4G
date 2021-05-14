b50fb0bce4370bed9d401a0e377315ec9110d78b
Valva <valvanuz.fernandez@unican.es>
2021-05-14 13:27:37 +0200

    DRM4G_DIR and WRF4G_DIR default values modified

--- wrf4g/__init__.py
+++ wrf4g/__init__.py
@@ -26,10 +26,10 @@ __version__  = '2.3.0'
 __author__   = 'Carlos Blanco'
 __revision__ = "$Id$"
 
-HOME                 = os.environ.get( 'HOME' )
-#WRF4G_DIR            = os.environ[ 'GW_LOCATION' ] = join( os.environ.get( 'WRF4G_DIR', HOME ), '.wrf4g' )
-WRF4G_DIR            = join( os.environ.get( 'WRF4G_DIR', HOME ), '.wrf4g' )
-os.environ[ 'DRM4G_DIR' ] =  os.environ.get( 'HOME' )
+HOME = os.environ.get("HOME")
+WRF4G_DIR = os.environ.get("WRF4G_DIR", "%s/.wrf4g" %(HOME))
+if ( not os.environ.get("DRM4G_DIR")):
+    os.environ["DRM4G_DIR"] =  "%s/.drm4g" %(HOME)
 #os.environ[ 'GW_LOCATION' ]= join( os.environ.get( 'DRM4G_DIR'), '.drm4g' )
 WRF4G_DEPLOYMENT_DIR = os.path.dirname(os.path.realpath(__file__))
 DB4G_CONF            = os.environ.get( 'DB4G_CONF',  join( WRF4G_DIR , 'etc' , 'db.conf' ) )
